4689dc337bf9c4e8f0ebb1d446005fdf2a7052c7
Thienhoangba99 <Thienhoangba99@users.noreply.github.com>
2019-10-27 13:16:50 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Nguyen_1minute_AD
+{
+    class shape
+    {
+        private double LongEdge;
+        private double ShortEdge;
+        public shape(double LongEdge, double ShortEdge)
+        {
+            this.LongEdge = LongEdge;
+            this.ShortEdge = ShortEdge;
+        }
+        public double calculateArea()
+        {
+            return LongEdge * ShortEdge;
+        }
+    }
+    class square : shape
+    {
+        public square(double LongEdge, double ShortEdge) : base(LongEdge, ShortEdge)
+        {
+
+        }
+    }
+    class rectangle : shape
+    {
+        public rectangle(double LongEdge, double ShortEdge) : base(LongEdge, ShortEdge)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            square sq = new square(20, 20);
+            Console.WriteLine("dien tich hinh vuong la: " + sq.calculateArea());
+            rectangle rec = new rectangle(30,60);
+            Console.WriteLine("dien tich hinh chu nhat la: " + rec.calculateArea());
+        }
+    }
+}
